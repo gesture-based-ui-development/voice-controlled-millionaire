2a50df70c0539f1994599573ef0890f5e57d35c4
kevin barry <g00339811@gmit.ie>
2019-04-03 19:42:43 +0100

    Started working on final answer

--- SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
+++ SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
 public class SpeechRecognitionEngine : MonoBehaviour
 {
     // Variables
-    private string[] keywords = new string[] { "a", "b", "c", "d", "quit", "pause", "new game" };
+    private string[] keywords = new string[] { "a", "b", "c", "d", "quit", "pause", "new game", "yes", "no", "final answer" };
     public ConfidenceLevel confidence = ConfidenceLevel.Low;
     public Text results;
     public Image target;
     static PhraseRecognizer recognizer;
     protected string word = "";
+
     public int questionsRight;
+
+    // Need to save the word as final answer word.
+    protected string finalAnswerWord = "";
+    protected bool answerIsFinal = false;
 
     SceneManagement sceneManager = new SceneManagement();
     //LoadQuestions loadQuestion = new LoadQuestions();
@@ -47,6 +50,51 @@ public class SpeechRecognitionEngine : MonoBehaviour
         switch (word)
         {
             case "a":
+                if (answerIsFinal)
+                {
+                    loadQuestion.checkAnswer(word);
+                }
+
+                break;
+            case "b":
+                if (answerIsFinal)
+                {
+                    loadQuestion.checkAnswer(word);
+                }
+
+                break;
+            case "c":
+                if (answerIsFinal)
+                {
+                    loadQuestion.checkAnswer(word);
+                }
+
+                break;
+            case "d":
+                if (answerIsFinal)
+                {
+                    loadQuestion.checkAnswer(word);
+                }
+
+                break;
+
+            case "quit":
+                break;
+            case "yes":
+                finalAnswerWord = word;
+                answerIsFinal = true;
+                break;
+            case "final answer":
+                answerIsFinal = true;
+                break;
+            case "no":
+                answerIsFinal = false;
+                break;
+            case "new game":
+                sceneManager.StartGame();
+                break;
+/*
+            case "a":
                 loadQuestion.checkAnswer(word);
                 break;
             case "b":
@@ -56,23 +104,38 @@ public class SpeechRecognitionEngine : MonoBehaviour
                 loadQuestion.checkAnswer(word);
                 break;
             case "d":
-                loadQuestion.checkAnswer(word);
+                .checkAnswer(word);
                 break;
+
             case "quit":
+                break;
                 break;
             case "new game":
                 sceneManager.StartGame();
                 break;
+
+*/
         }
-    }
-    private void OnApplicationQuit()
-    {
-        if (recognizer != null && recognizer.IsRunning)
+        }
+        private void OnApplicationQuit()
         {
-            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
-            recognizer.Stop();
+            if (recognizer != null && recognizer.IsRunning)
+            {
+                recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+                recognizer.Stop();
+            }
         }
+
+        /*
+         * Prompt yes/no button dialog to confirm if final answwer
+        */
+        private void IsFinalAnswer(string word)
+        {
+            // if the answer is yes
+            // then loadQuestion.checkAnswer(word)
+            // else take in another word
+
+        }
+
+
     }
-
-
-}
